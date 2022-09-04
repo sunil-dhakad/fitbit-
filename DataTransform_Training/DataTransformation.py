fe12582dbfbc3c029afe951f7598509604478ceb
sunil Nagar <0to1.sunil@gmail.com>
2022-09-04 16:25:22 +0530

    change in data tranform class

--- DataTransform_Training/DataTransformation.py
+++ DataTransform_Training/DataTransformation.py
@@ -9,7 +9,6 @@ class dataTransform:
      """
                This class shall be used for transforming the Good Raw Training Data before loading it in Database!!.
 
-               Written By: iNeuron Intelligence
                Version: 1.0
                Revisions: None
 
@@ -27,7 +26,6 @@ class dataTransform:
                                                        each value for that column is enclosed in quotes. This is done
                                                        to avoid the error while inserting string values in table as varchar.
 
-                                            Written By: iNeuron Intelligence
                                            Version: 1.0
                                            Revisions: None
 
@@ -65,7 +63,6 @@ class dataTransform:
      #                                                Method Name: addQuotesToStringValuesInColumn
      #                                                Description: This method changing the column names by replacing the '-'.
      #
-     #                                                 Written By: iNeuron Intelligence
      #                                                Version: 1.0
      #                                                Revisions: None
      #
